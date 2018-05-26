cff2b627c64eefa051d87b045e829bea894dc1b9
zhangnan <zn@zn-PC>
2018-05-26 21:07:17 +0800

    "new barth "

--- src/com/home/Hello.java
+++ src/com/home/Hello.java
@@ -7,6 +7,7 @@ public class Hello {
 		System.out.println("hello world2");
 		System.out.println("hello world2");
 		System.out.println("hello world2");
+		System.out.println("hello world2");
 	}
 	
 }
